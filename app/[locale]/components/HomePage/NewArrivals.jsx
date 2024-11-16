deb7bc3878d474807761910cd9a0eb724855d715
andriadze66 <giorgiandriadze814@gmail.com>
2024-11-16 13:03:03 +0400

    Revert "1.create custom hook fo slider"
    This reverts commit e1824a22786fe30334807c16637b4493759b6c6b.

--- app/[locale]/components/HomePage/NewArrivals.jsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import "../../assets/css/NewHomePage.css"
-
-
-
-
--- app/[locale]/components/HomePage/NewHomePage.jsx
+++ app/[locale]/components/HomePage/NewHomePage.jsx
@@ -7,22 +7,12 @@ import BillboardImg4 from "../../assets/img/homePageImg/NewHomePage/Header-Img4.
 import BillboardImg5 from "../../assets/img/homePageImg/NewHomePage/Header-Img5.png"
 import KidsTrending from "../../assets/img/homePageImg/Trending/Kids.png"
 
-import TrendingSlider from "./TrendingSlider";
+import Slider from "./Slider";
+
+
 import WomanTrending from "../../assets/img/homePageImg/Trending/WomanTrend.png"
 
 import SwimWear from '../../assets/img/homePageImg/NewHomePage/SwimWear.png'
-import Link from "next/link";
-
-import MainBanner from '../../assets/img/homePageImg/NewArrival/MainBanner.png'
-import NewArrival11 from '../../assets/img/homePageImg/NewArrival/NewArrival1-1.png'
-import NewArrival12 from '../../assets/img/homePageImg/NewArrival/NewArrival1-2.png'
-import NewArrival21 from '../../assets/img/homePageImg/NewArrival/NewArrival2-1.png'
-import NewArrival22 from '../../assets/img/homePageImg/NewArrival/NewArrival2-2.png'
-import NewArrival31 from '../../assets/img/homePageImg/NewArrival/NewArrival3-1.png'
-import NewArrival32 from '../../assets/img/homePageImg/NewArrival/NewArrival3-2.png'
-import NewArrival41 from '../../assets/img/homePageImg/NewArrival/NewArrival4-1.png'
-import NewArrival42 from '../../assets/img/homePageImg/NewArrival/NewArrival4-2.png'
-
 // async function GetHomePageData(params) {
 //     const [woman, man] = await Promise.all([
 //         fetch('https://dummyjson.com/products/category/womens-dresses'),
@@ -36,7 +26,7 @@ import NewArrival42 from '../../assets/img/homePageImg/NewArrival/NewArrival4-2.
 
 async function HomePage(){
     // const data = await GetHomePageData();
-    console.log(MainBanner)
+
     return(
         <>
             <div className="mainBillboard">
@@ -100,40 +90,7 @@ async function HomePage(){
                 </div>
 
             </div>
-            <TrendingSlider></TrendingSlider>
-            <div className="BannerWrapper">
-                <div className="BannerContainer">
-                    <Link className="mainBanner" href='/'>
-                        <div className="mainBanner-ImgDiv">
-                            <Image height={500} width={500} alt="" src={MainBanner}/>
-                        </div>
-                        <button className="exploreButton">Explore</button>
-                    </Link>
-                    <div className="sliderWrapper">
-                        <div className="sliderContainer">
-                            <div style={{display:"flex", justifyContent:"space-between"}}>
-                                <h3>New Arrivals</h3>
-                                <div style={{display:"flex", gap:"20px"}}>
-                                    <button class="lineButton">view all collections</button>
-                                    <div style={{display:"flex",gap:"10px", justifyContent:"center"}}>
-                                        <button className="slider-Button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-                                            </svg>
-                                        </button>
-                                        <button className="slider-Button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
+            <Slider></Slider>
         </>
     )
 }
