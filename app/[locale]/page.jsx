9a4f7c48fe8d913eef397d42dd490fb70cb03b95
andriadze66 <giorgiandriadze814@gmail.com>
2025-01-15 23:43:31 +0400

    ზედმეტი ხაზი

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,6 +1,5 @@
 
 import {useTranslations} from 'next-intl';
-import Header from "./components/OldHeader";
 import NewHeader from './components/Header/NewHeader';
 import NewHomePage from './components/HomePage/NewHomePage'
 import '/app/globals.css'
