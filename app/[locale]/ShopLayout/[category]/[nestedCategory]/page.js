ff397ae29976fa9754ee737106a9de50e7eaed25
andriadze66 <giorgiandriadze814@gmail.com>
2025-01-17 12:59:15 +0400

    წავშალ დამატებით ფაილები

--- app/[locale]/ShopLayout/[category]/[nestedCategory]/page.js
+++
@@ -1,9 +0,0 @@
-import '../../assets/css/shopLayout.css'
-import SearchProducts from '@/app/[locale]/components/SearchProducts'
- export default function ShopLayout(props){
-    return(
-        <>
-            <SearchProducts parametrs={props}></SearchProducts>
-        </>
-    )
-}
