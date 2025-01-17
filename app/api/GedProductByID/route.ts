ba1b5d0d7063ccac51ca89e6ba7352c9f317a578
andriadze66 <giorgiandriadze814@gmail.com>
2025-01-17 13:01:04 +0400

    პროდუქტის ინფორმაცია ID დაყრდნობით

--- app/api/GedProductByID/route.ts
+++ app/api/GedProductByID/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from '../../lib/supaBase/server'
 import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
-import { productDetailedType,GlobalProductInfoType, productColorsType, productStockType} from '../../Type/type'
+import {GlobalProductInfoType, productColorsType, productStockType, ImagesType} from '../../Type/type'
 
 /**
  * POST method to handle adding a product
@@ -11,51 +11,69 @@ import { productDetailedType,GlobalProductInfoType, productColorsType, productSt
 
 export async function POST(request: NextRequest) {
     try{
-        let catalogArray = new Array<productDetailedType>();
-        let globalInfo = {} as  GlobalProductInfoType;
         const supabase = await createClient();
-        const formData = await request.formData();
-        const id = formData.get('id') as string;
-        const { data:GlobalProductInfo, error:globalInfoError } = await supabase.from('GlobalProductInfo').select('*').eq('product_id', id);
-        if (globalInfoError) {
-            return NextResponse.json({ error: globalInfoError.message }, { status: 500 });
-        }
-        globalInfo = GlobalProductInfo[0];
+        const body = await request.json();
+        const id = body.id;
 
-        let { data: productColors, error } = await supabase
-        .from('productColors')
-        .select('*')
-        .eq('product_id', id);
-        if (error) {
-            return NextResponse.json({ error: error.message }, { status: 500 });
-        }
-        for (let i = 0; i < productColors.length; i++) {
-            let  currentProductColor = productColors[i] as productColorsType;
-            let { data: productStock, error:productStockError } = await supabase
+
+        const {data: productColorsResult, error: productColorsError} = await supabase
+        .from('productColors').select('*')
+        .eq('productColorID', id);
+
+        const [GlobalProductInfoResult,productStockResult, imagesResult] = await Promise.all([
+            supabase
+            .from('GlobalProductInfo')
+            .select('*')
+            .eq('product_id', productColorsResult[0].product_id),
+            supabase
                 .from('productStock')
                 .select("*")
-                // Filters
-                .eq('product_colorID', currentProductColor.productColorID);
-            if (productStockError) {
-                return NextResponse.json({ error: productStockError.message }, { status: 500 });
-            }
-            let currentSize = productStock as productStockType[];
-
-            let { data: Images, error:imagesError } = await supabase
+                .eq('product_id', productColorsResult[0].product_id),
+            supabase
                 .from('Images')
-                .select('imageURL')
-                .eq("productColorID", currentProductColor.productColorID);
-            if (imagesError) {
-                return NextResponse.json({ error: imagesError.message }, { status: 500 });
-            }
-            let currentImage = JSON.parse(JSON.stringify(Images)) as string[];
-            catalogArray.push({
-                productColors: currentProductColor,
-                productStock: currentSize,
-                Image: currentImage
-            })
+                .select('imageURL, productColorID, isPrimary')
+                .eq("product_id", productColorsResult[0].product_id)
+        ]);
+
+        if (GlobalProductInfoResult.error) {
+            return NextResponse.json({ error: GlobalProductInfoResult.error.message }, { status: 500 });
         }
-        return NextResponse.json(catalogArray);
+        if (productColorsError) {
+            return NextResponse.json({ error: productColorsError.message }, { status: 500 });
+        }
+
+        if (productStockResult.error) {
+            return NextResponse.json({ error: productStockResult.error.message }, { status: 500 });
+        }
+
+        if (imagesResult.error) {
+            return NextResponse.json({ error: imagesResult.error.message }, { status: 500 });
+        }
+
+        const globalProductInfo = GlobalProductInfoResult.data as GlobalProductInfoType[];
+        const productColors = productColorsResult as productColorsType[];
+        const productStock = {}
+        productStockResult.data.map((element) => {
+            let product_ColorID = element.product_ColorID;
+            let currentSize = element.size;
+            let sizeObject = {}
+            sizeObject[currentSize] = {
+                price_lari: element.price_lari,
+                count: element.count,
+                price_usd: element.price_usd,
+                stripe_ProductID: element.stripe_ProductID,
+                productStockID: element.productStockID
+            }
+            productStock[product_ColorID] = {
+                ...productStock[product_ColorID],
+                ...sizeObject
+            }
+        })
+        // const productStock = productStockResult.data as productStockType[];
+
+        const images = imagesResult.data as ImagesType[];
+
+        return NextResponse.json({globalProductInfo,productColors,productStock,images});
     }
     catch(error){
         console.log(error)
